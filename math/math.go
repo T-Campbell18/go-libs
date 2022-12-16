304a07de82c488c39c7efdef5b2d9c48c0311f42
Tyler Campbell <tccsports18@gmail.com>
2022-12-16 17:57:32 -0600

    pi, e, round, pow

--- math/math.go
+++ math/math.go
@@ -6,6 +6,9 @@ type Number interface {
 	constraints.Float | constraints.Integer
 }
 
+const PI = 3.14159265358979323846264338327950288419716939937510582097494459
+const E = 2.71828182845904523536028747135266249775724709369995957496696763
+
 func Min[T Number](a, b T) T {
 	if a < b {
 		return a
@@ -33,4 +36,26 @@ func Ceil[T constraints.Float](a T) T {
 
 func Floor[T constraints.Float](a T) T {
 	return T(int(a))
+}
+
+func Round[T constraints.Float](a T) T {
+	return T(int(a + 0.5))
+}
+
+func Pow[T Number](x T, n int) T {
+	var pow int64 = int64(n)
+	if pow < 0 {
+		pow = -pow
+		x = 1 / x
+	}
+
+	var ans T = 1
+	var curr T = x
+	for i := pow; i > 0; i /= 2 {
+		if i%2 == 1 {
+			ans *= curr
+		}
+		curr *= curr
+	}
+	return ans
 }
